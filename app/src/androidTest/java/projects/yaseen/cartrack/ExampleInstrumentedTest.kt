9b4981454ab018d2b8c1da94fdc26e463e5d5add
Mohammed Yaseen <yaseen@homage.co>
2019-02-16 22:44:53 +0800

    Project initial setup

---
+++ app/src/androidTest/java/projects/yaseen/cartrack/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package projects.yaseen.cartrack
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("projects.yaseen.cartrack", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/projects/yaseen/cartrack/MainActivity.kt
@@ -0,0 +1,12 @@
+package projects.yaseen.cartrack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
